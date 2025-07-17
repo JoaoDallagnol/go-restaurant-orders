6490b177a53e05293150f271e763086dd6c39ed1
JoaoDallagnol <jvdallagnol2001@gmail.com>
2025-07-16 21:31:13 -0300

    Creating the struct of user object

---
+++ auth-service/internal/model/user.go
@@ -0,0 +1,35 @@
+package model
+
+import "time"
+
+type User struct {
+	ID        uint `gorm:"primaryKey"`
+	Name      string
+	Email     string
+	Password  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type RegisterUserRequest struct {
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=3"`
+}
+
+type UserResponse struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	CreatedAt string `json:"created_at"`
+}
+
+type UserLoginRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=3"`
+}
+
+type UserLoginResponse struct {
+	Token     string `json:"token"`
+	ExpiresIn string `json:"expires_in"`
+}
