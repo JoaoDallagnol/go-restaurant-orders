25e923256d4629810c40aa80938d96abeb225720
JoaoDallagnol <jvdallagnol2001@gmail.com>
2025-07-22 15:07:46 -0300

    Refactoring the user_repo to better struct

--- auth-service/internal/repository/user_repository.go
+++ auth-service/internal/repository/user_repository.go
@@ -1,32 +1,40 @@
 package repository
 
 import (
-	"github.com/JoaoDallagnol/go-restaurant-orders/auth-service/db"
 	"github.com/JoaoDallagnol/go-restaurant-orders/auth-service/internal/model"
+	"gorm.io/gorm"
 )
 
-func CreateUser(user *model.User) error {
-	return db.DB.Create(user).Error
+type UserRepository struct {
+	db *gorm.DB
 }
 
-func GetUserById(id uint) (*model.User, error) {
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) CreateUser(user *model.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *UserRepository) GetUserById(id uint) (*model.User, error) {
 	var user model.User
-	if err := db.DB.First(&user, id).Error; err != nil {
+	if err := r.db.First(&user, id).Error; err != nil {
 		return nil, err
 	}
 
 	return &user, nil
 }
 
-func GetAllUsers() ([]model.User, error) {
+func (r *UserRepository) GetAllUsers() ([]model.User, error) {
 	var users []model.User
-	if err := db.DB.Find(&users).Error; err != nil {
+	if err := r.db.Find(&users).Error; err != nil {
 		return nil, err
 	}
 
 	return users, nil
 }
 
-func DeleteUser(id uint) error {
-	return db.DB.Delete(&model.User{}, id).Error
+func (r *UserRepository) DeleteUser(id uint) error {
+	return r.db.Delete(&model.User{}, id).Error
 }
