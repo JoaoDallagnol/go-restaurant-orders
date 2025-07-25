f818353eaddaaaba857e281df33e9724ebc7930b
JoaoDallagnol <jvdallagnol2001@gmail.com>
2025-07-25 16:45:18 -0300

    Creating a mapper class to centralize all the mappers funcions and refactoring the auth service to call the mapper methods

---
+++ auth-service/internal/mapper/mapper.go
@@ -0,0 +1,20 @@
+package mapper
+
+import "github.com/JoaoDallagnol/go-restaurant-orders/auth-service/internal/model"
+
+func MapCreateUserRequestToUser(dto *model.RegisterUserRequest) model.User {
+	return model.User{
+		Name:     dto.Name,
+		Email:    dto.Email,
+		Password: dto.Password,
+	}
+}
+
+func MapUserToUserResponse(user *model.User) model.UserResponse {
+	return model.UserResponse{
+		ID:        user.ID,
+		Name:      user.Name,
+		Email:     user.Email,
+		CreatedAt: user.CreatedAt.String(),
+	}
+}
--- auth-service/internal/service/auth_service.go
+++ auth-service/internal/service/auth_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"github.com/JoaoDallagnol/go-restaurant-orders/auth-service/internal/mapper"
 	"github.com/JoaoDallagnol/go-restaurant-orders/auth-service/internal/model"
 	"github.com/JoaoDallagnol/go-restaurant-orders/auth-service/internal/repository"
 )
@@ -19,33 +20,16 @@ func NewAuthService(userRepository repository.UserRepository) AuthService {
 }
 
 func (s *authService) RegisterUser(userReq *model.RegisterUserRequest) model.UserResponse {
-	user := mapCreateUserRequestToUser(userReq)
+	user := mapper.MapCreateUserRequestToUser(userReq)
 
 	createdUser, err := s.userRepository.CreateUser(&user)
 	if err != nil {
 		panic("Falied to create user: " + err.Error())
 	}
 
-	return mapUserToUserResponse(createdUser)
+	return mapper.MapUserToUserResponse(createdUser)
 }
 
 func (s *authService) Login(loginReq *model.UserLoginRequest) string {
 	return "Usuario Logado!"
-}
-
-func mapCreateUserRequestToUser(dto *model.RegisterUserRequest) model.User {
-	return model.User{
-		Name:     dto.Name,
-		Email:    dto.Email,
-		Password: dto.Password,
-	}
-}
-
-func mapUserToUserResponse(user *model.User) model.UserResponse {
-	return model.UserResponse{
-		ID:        user.ID,
-		Name:      user.Name,
-		Email:     user.Email,
-		CreatedAt: user.CreatedAt.String(),
-	}
 }
