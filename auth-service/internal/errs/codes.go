2c4675ec9c05332d967057e3e32b6970941dc089
JoaoDallagnol <jvdallagnol2001@gmail.com>
2025-08-01 22:05:48 -0300

    Correcting the typo of erros consts

--- auth-service/internal/errs/codes.go
+++ auth-service/internal/errs/codes.go
@@ -4,6 +4,6 @@ type ErrorCode string
 
 const (
 	CodeUserNotFound     ErrorCode = "USER_NOT_FOUND"
-	CodeInternalError    ErrorCode = "INTERNAL+ERROR"
+	CodeInternalError    ErrorCode = "INTERNAL_ERROR"
 	CodeAuthInvalidCreds ErrorCode = "AUTH_INVALID_CREDENTIALS"
 )
