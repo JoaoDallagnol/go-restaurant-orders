96b281675faa056b52bfd47f390bb65dd0307f41
JoaoDallagnol <jvdallagnol2001@gmail.com>
2025-08-09 10:29:35 -0300

    Calling the restaurant_service in the restaurant_handler

--- menu-service/internal/handlers/restaurant_handler.go
+++ menu-service/internal/handlers/restaurant_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/JoaoDallagnol/go-restaurant-orders/menu-service/internal/model"
 	"github.com/JoaoDallagnol/go-restaurant-orders/menu-service/internal/service"
 	"github.com/gin-gonic/gin"
 )
@@ -16,21 +17,63 @@ func NewRestaurantHandler(restaurantService service.RestaurantService) *Restaura
 }
 
 func (h *RestaurantHandler) GetAllRestaurants(c *gin.Context) {
-	c.JSON(http.StatusOK, "Restaurant List")
+	response, err := h.restaurantService.GetAllRestaurants()
+	if err != nil {
+		panic("error: " + err.Error())
+	}
+
+	c.JSON(http.StatusOK, response)
 }
 
 func (h *RestaurantHandler) GetRestaurantById(c *gin.Context) {
-	c.JSON(http.StatusOK, "Restaurant by Id")
+	id := c.Param("id")
+	response, err := h.restaurantService.GetRestaurantById(id)
+	if err != nil {
+		panic("error: " + err.Error())
+	}
+
+	c.JSON(http.StatusOK, response)
 }
 
 func (h *RestaurantHandler) CreateRestaurant(c *gin.Context) {
-	c.JSON(http.StatusCreated, "Restaurant Created")
+	var restaurantReq model.RestaurantRequest
+	if err := c.ShouldBindJSON(&restaurantReq); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	response, err := h.restaurantService.CreateRestaurant(&restaurantReq)
+	if err != nil {
+		panic("error: " + err.Error())
+	}
+
+	c.JSON(http.StatusCreated, response)
 }
 
 func (h *RestaurantHandler) UpdateRestaurant(c *gin.Context) {
-	c.JSON(http.StatusOK, "Restaurant Updated")
+	id := c.Param("id")
+	var restaurantReq model.RestaurantRequest
+
+	if err := c.ShouldBindJSON(&restaurantReq); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	response, err := h.restaurantService.UpdateRestaurant(id, &restaurantReq)
+	if err != nil {
+		panic("error: " + err.Error())
+	}
+
+	c.JSON(http.StatusOK, response)
 }
 
 func (h *RestaurantHandler) DeleteRestaurant(c *gin.Context) {
-	c.JSON(http.StatusNoContent, "Restaurant Deleted")
+	id := c.Param("id")
+	err := h.restaurantService.DeleteRestaurant(id)
+
+	if err != nil {
+		panic("error: " + err.Error())
+	}
+
+	c.JSON(http.StatusNoContent, model.DishResponse{})
 }
