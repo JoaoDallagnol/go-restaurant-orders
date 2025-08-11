86a23b64afd39c0e88c7a30893dc9f4b331918e5
JoaoDallagnol <jvdallagnol2001@gmail.com>
2025-08-11 15:18:19 -0300

    Calling the dish_service in the dish_handler

--- menu-service/internal/handlers/dish_handler.go
+++ menu-service/internal/handlers/dish_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/JoaoDallagnol/go-restaurant-orders/menu-service/internal/model"
 	"github.com/JoaoDallagnol/go-restaurant-orders/menu-service/internal/service"
 	"github.com/gin-gonic/gin"
 )
@@ -16,21 +17,61 @@ func NewDishHandler(dishService service.DishService) *DishHandler {
 }
 
 func (h *DishHandler) GetAllDishes(c *gin.Context) {
-	c.JSON(http.StatusOK, "Dishes List")
+	response, err := h.dishService.GetAllDishes()
+	if err != nil {
+		panic("error: " + err.Error())
+	}
+
+	c.JSON(http.StatusOK, response)
 }
 
 func (h *DishHandler) GetDishById(c *gin.Context) {
-	c.JSON(http.StatusOK, "Dish by Id")
+	id := c.Param("id")
+	response, err := h.dishService.GetDishById(id)
+	if err != nil {
+		panic("error: " + err.Error())
+	}
+
+	c.JSON(http.StatusOK, response)
 }
 
 func (h *DishHandler) CreateDish(c *gin.Context) {
-	c.JSON(http.StatusCreated, "Dish Ureated")
+	var dishReq model.DishRequest
+	if err := c.ShouldBindJSON(&dishReq); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	response, err := h.dishService.CreateDish(&dishReq)
+	if err != nil {
+		panic("error: " + err.Error())
+	}
+
+	c.JSON(http.StatusCreated, response)
 }
 
 func (h *DishHandler) UpdateDish(c *gin.Context) {
-	c.JSON(http.StatusOK, "Dish Updated")
+	id := c.Param("id")
+	var dishReq model.DishRequest
+	if err := c.ShouldBindJSON(&dishReq); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	response, err := h.dishService.UpdateDish(id, &dishReq)
+	if err != nil {
+		panic("error: " + err.Error())
+	}
+
+	c.JSON(http.StatusOK, response)
 }
 
 func (h *DishHandler) DeleteDish(c *gin.Context) {
-	c.JSON(http.StatusNoContent, "Dish Deleted")
+	id := c.Param("id")
+	err := h.dishService.DeleteDish(id)
+	if err != nil {
+		panic("error: " + err.Error())
+	}
+
+	c.JSON(http.StatusNoContent, model.DishResponse{})
 }
