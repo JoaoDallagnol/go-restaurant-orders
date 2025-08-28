069b9cb4298f949579dc8ebb0c71140422d7b4c3
JoaoDallagnol <jvdallagnol2001@gmail.com>
2025-08-28 11:06:03 -0300

    Importing and implementing viper to load config from a yml file

--- order-service/cmd/main.go
+++ order-service/cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/JoaoDallagnol/go-restaurant-orders/order-service/internal/config"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	config.LoadConfig()
 	server := gin.Default()
 	server.Run(":8082")
 }
