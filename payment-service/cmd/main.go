efabc00ca1a6907844c1f8563545d0ced46c511c
JoaoDallagnol <jvdallagnol2001@gmail.com>
2025-10-27 15:09:36 -0300

    Creating main file and declaring dependencies

---
+++ payment-service/cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/JoaoDallagnol/go-restaurant-orders/payment-service/db"
+	"github.com/JoaoDallagnol/go-restaurant-orders/payment-service/internal/client"
+	"github.com/JoaoDallagnol/go-restaurant-orders/payment-service/internal/config"
+	"github.com/JoaoDallagnol/go-restaurant-orders/payment-service/internal/handlers"
+	"github.com/JoaoDallagnol/go-restaurant-orders/payment-service/internal/repository"
+	"github.com/JoaoDallagnol/go-restaurant-orders/payment-service/internal/routers"
+	"github.com/JoaoDallagnol/go-restaurant-orders/payment-service/internal/service"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	config.LoadConfig()
+	db.Init()
+
+	orderClient := client.NewOrderClient(config.AppConfig)
+	paymentRepository := repository.NewPaymentRepository(db.DB)
+	paymentService := service.NewPaymentService(paymentRepository, orderClient)
+	paymentHandler := handlers.NewPaymentHandler(paymentService)
+
+	server := gin.Default()
+	routers.RegisterRoutes(server, paymentHandler)
+
+	port := config.AppConfig.Server.Port
+	server.Run(fmt.Sprintf(":%d", port))
+}
