384666377fac02b1405214c7c7c07813839d3d8a
JoaoDallagnol <jvdallagnol2001@gmail.com>
2025-11-02 10:52:47 -0300

    Initializing the consumer in the main.go file

--- order-service/cmd/main.go
+++ order-service/cmd/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/JoaoDallagnol/go-restaurant-orders/order-service/db"
 	"github.com/JoaoDallagnol/go-restaurant-orders/order-service/internal/client"
 	"github.com/JoaoDallagnol/go-restaurant-orders/order-service/internal/config"
+	"github.com/JoaoDallagnol/go-restaurant-orders/order-service/internal/consumers"
 	"github.com/JoaoDallagnol/go-restaurant-orders/order-service/internal/handlers"
 	"github.com/JoaoDallagnol/go-restaurant-orders/order-service/internal/repository"
 	"github.com/JoaoDallagnol/go-restaurant-orders/order-service/internal/routers"
@@ -20,14 +22,25 @@ func main() {
 	menuClient := client.NewMenuClient(config.AppConfig)
 	authClient := client.NewAuthClient(config.AppConfig)
 
-	orderRespository := repository.NewOrderRepository(db.DB)
+	orderRepository := repository.NewOrderRepository(db.DB)
 	orderItemRepository := repository.NewOrderItemRepository(db.DB)
 
-	orderService := service.NewOrderService(orderRespository, orderItemRepository, menuClient, authClient)
+	orderService := service.NewOrderService(orderRepository, orderItemRepository, menuClient, authClient)
 	orderItemService := service.NewOrderItemService(orderItemRepository)
 
 	orderHandler := handlers.NewOrderHandler(orderService)
 	orderItemHandler := handlers.NewOrderItemHandler(orderItemService)
+
+	consumer, err := consumers.NewOrderStatusConsumer(config.AppConfig, orderRepository)
+	if err != nil {
+		log.Fatalf("❌ Failed to initialize RabbitMQ consumer: %v", err)
+	}
+
+	go func() {
+		if err := consumer.Start(); err != nil {
+			log.Fatalf("❌ Consumer crashed: %v", err)
+		}
+	}()
 
 	server := gin.Default()
 	routers.RegisterRoutes(server, orderHandler, orderItemHandler)
